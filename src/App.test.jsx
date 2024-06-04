747da2a70c2899b4b154085c5b6c1c30447d200d
Anya Aven <leafeon96@gmail.com>
2024-06-04 15:01:04 -0400

    Deleted junk info

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,20 +4,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App.jsx';
 
 
-test('renders counter button', function () {
+test('renders', function () {
   render(<App />);
-  const buttonElement = screen.getByText(/Clicked: 0/i);
-  expect(buttonElement).toBeInTheDocument();
-});
 
-test('count increases on button click', function () {
-  render(<App />);
-  const buttonElement = screen.getByText(/Clicked: 0/i);
-  fireEvent.click(buttonElement);
-  expect(buttonElement).toHaveTextContent(/Clicked: 1/i);
-});
-
-test('matches snapshot', function () {
-  const { container } = render(<App />);
-  expect(container).toMatchSnapshot();
 });
